497e9c03e505840e3bda8d3bdb56fbed7f93bb8b
ChintanR <rokadechintan2005@gmail.com>
2025-11-20 13:20:59 +0530

    Add files via upload

---
+++ Check_length.py
@@ -0,0 +1,6 @@
+a = input("Enter username: ")
+
+if(len(a)<10):
+    print("Given username contains less than 10 characters")
+else:
+    print("Given username doesn't contains less than 10 characters")
