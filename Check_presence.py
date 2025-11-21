f61456048f09d3c58ac83235ade24fd43fc7bc7a
ChintanR <rokadechintan2005@gmail.com>
2025-11-21 10:43:44 +0530

    Add files via upload

---
+++ Check_presence.py
@@ -0,0 +1,7 @@
+a = ["chintan", "sonam", "mithi", "aman"]
+b = str(input("Enter Name: "))
+
+if(b in a):
+    print("Given name is present in the list")
+else:
+    print("Given name is not present in the list")
