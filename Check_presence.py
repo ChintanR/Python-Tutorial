f75f49f398a0bb5f67eee7f51106090f58ba8de8
ChintanR <rokadechintan2005@gmail.com>
2026-01-10 15:50:47 +0530

    Delete Check_presence.py

--- Check_presence.py
+++
@@ -1,7 +0,0 @@
-a = ["chintan", "sonam", "mithi", "aman"]
-b = str(input("Enter Name: "))
-
-if(b in a):
-    print("Given name is present in the list")
-else:
-    print("Given name is not present in the list")
